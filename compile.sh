106547c6dd67988bfd751e6794c5189cf980622e
Michael <34433998+micneu72@users.noreply.github.com>
2021-04-16 07:58:11 +0200

    Update compile.sh
    kleine fehler korrigiert

--- compile.sh
+++ compile.sh
@@ -2,21 +2,29 @@
 mkdir -p build/macOS_intel/
 mkdir -p build/win/
 mkdir -p build/linux_intel/
-mkdir -p build/linux_arm/
+mkdir -p build/linux_arm32/
+mkdir -p build/linux_arm64/
 mkdir -p build/freebsd/
 export GOOS=darwin
 export GOARCH=amd64
 go build -o build/macOS_intel/
+export GOOS=darwin
+export GOARCH=arm64
+go build -o build/macOS_m1/
 export GOOS=windows
 export GOARCH=amd64
 go build -o build/win/
 export GOOS=linux
-export GPARCH=amd64
+export GOARCH=amd64
 go build -o build/linux_intel/
 export GOOS=linux
-export GPARCH=arm
+export GOARCH=arm64
 export GOARM=7
-go build -o build/linux_arm/
+go build -o build/linux_arm64/
+export GOOS=linux
+export GOARCH=arm
+export GOARM=7
+go build -o build/linux_arm32/
 export GOOS=freebsd
 export GOARCH=amd64
 go build -o build/freebsd/
