70dfebc06cbbd1a5a30b5470c7bebcff3e26fe2a
Michael Neumann <mne@micneu.net>
2021-04-17 03:02:23 +0200

    psk wird richtig gesetzt

--- main.go
+++ main.go
@@ -40,6 +40,7 @@ func genPSK() string {
 }
 
 func main() {
+	PSK := genPSK()
 
 	//Angaben Seite A
 	nameSeiteAPtr := flag.String("aname", "Seite A", "Name der Seite A")
@@ -52,94 +53,90 @@ func main() {
 	DYNDNSB := flag.String("bdns", "b.myfritz.net", "Dyndns Seite B")
 	pconf := flag.Bool("P", false, "VPN Konfig anzeigen")
 
-	PSK := genPSK()
-	printSetup(*nameSeiteAPtr, *ipSeiteAPtr, *DYNDNSA)
-	printSetup(*nameSeiteBPtr, *ipSeiteBPtr, *DYNDNSB)
-
 	flag.Parse()
 
 	alipsec_conf := `conn ###NAME###
-	       aggressive = yes
-	       fragmentation = yes
-	       keyexchange = ikev1
-	       mobike = yes
-	       reauth = yes
-	       rekey = yes
-	       forceencaps = no
-	       installpolicy = yes
-	       type = tunnel
-	       dpdaction = restart
-	       dpddelay = 10s
-	       dpdtimeout = 60s
+	aggressive = yes
+	fragmentation = yes
+	keyexchange = ikev1
+	mobike = yes
+	reauth = yes
+	rekey = yes
+	forceencaps = no
+	installpolicy = yes
+	type = tunnel
+	dpdaction = restart
+	dpddelay = 10s
+	dpdtimeout = 60s
 
-	       left=###DYNDNSA###
-	       #left = %any
-	       right = ###DYNDNSB###
+	left=###DYNDNSA###
+	#left = %any
+	right = ###DYNDNSB###
 
-	       leftid = ###DYNDNSA###
-	       ikelifetime = 3600s
-	       lifetime = 3600s
-	       ike = aes256-sha512-modp1024!
-	       leftauth = psk
-	       rightauth = psk
-	       rightid = @###DYNDNSB###
-	       reqid = 1
-	       rightsubnet = ###IPSEITEB###/24
-	       leftsubnet = ###IPSEITEA###/24
-	       esp = aes256-sha512-modp1024!
-	       auto = route
+	leftid = ###DYNDNSA###
+	ikelifetime = 3600s
+	lifetime = 3600s
+	ike = aes256-sha512-modp1024!
+	leftauth = psk
+	rightauth = psk
+	rightid = @###DYNDNSB###
+	reqid = 1
+	rightsubnet = ###IPSEITEB###/24
+	leftsubnet = ###IPSEITEA###/24
+	esp = aes256-sha512-modp1024!
+	auto = route
 
 	   `
 
 	bfipsec_conf := `vpncfg {
-	   	connections {
-	   		enabled = yes;
-	   		editable = yes;
-	   		conn_type = conntype_lan;
-	   		name = "###NAME###";
-	   		boxuser_id = 0;
-	   		always_renew = yes;
-	   		reject_not_encrypted = no;
-	   		dont_filter_netbios = yes;
-	   		localip = 0.0.0.0;
-	   		local_virtualip = 0.0.0.0;
-	   		remotehostname = "###DYNDNSA###";
-	   		remote_virtualip = 0.0.0.0;
-	   		localid {
-	   			fqdn = "###DYNDNSB###";
-	   		}
-	   		remoteid {
-	   			fqdn = "###DYNDNSA###";
-	   		}
-	   		mode = phase1_mode_idp;
-	   		phase1ss = "all/all/all";
-	   		keytype = connkeytype_pre_shared;
-	   		key = "###PSK###";
-	   		cert_do_server_auth = no;
-	   		use_nat_t = yes;
-	   		use_xauth = no;
-	   		use_cfgmode = no;
-	   		phase2localid {
-	   			ipnet {
-	   				ipaddr = ###IPSEITEB###;
-	   				mask = 255.255.255.0;
-	   			}
-	   		}
-	   		phase2remoteid {
-	   			ipnet {
-	   				ipaddr = ###IPSEITEA###;
-	   				mask = 255.255.255.0;
-	   			}
-	   		}
-	   		phase2ss = "esp-all-all/ah-none/comp-all/pfs";
-	   		accesslist = "permit ip any ###IPSEITEA### 255.255.255.0";
-	   	}
-	   	ike_forward_rules = "udp 0.0.0.0:500 0.0.0.0:500",
-	   			"udp 0.0.0.0:4500 0.0.0.0:4500";
+	connections {
+		enabled = yes;
+		editable = yes;
+		conn_type = conntype_lan;
+		name = "###NAME###";
+		boxuser_id = 0;
+		always_renew = yes;
+		reject_not_encrypted = no;
+		dont_filter_netbios = yes;
+		localip = 0.0.0.0;
+		local_virtualip = 0.0.0.0;
+		remotehostname = "###DYNDNSA###";
+		remote_virtualip = 0.0.0.0;
+		localid {
+			fqdn = "###DYNDNSB###";
+		}
+		remoteid {
+			fqdn = "###DYNDNSA###";
+		}
+		mode = phase1_mode_idp;
+		phase1ss = "all/all/all";
+		keytype = connkeytype_pre_shared;
+		key = "###PSK###";
+		cert_do_server_auth = no;
+		use_nat_t = yes;
+		use_xauth = no;
+		use_cfgmode = no;
+		phase2localid {
+			ipnet {
+				ipaddr = ###IPSEITEB###;
+				mask = 255.255.255.0;
+			}
+		}
+		phase2remoteid {
+			ipnet {
+				ipaddr = ###IPSEITEA###;
+				mask = 255.255.255.0;
+			}
+		}
+		phase2ss = "esp-all-all/ah-none/comp-all/pfs";
+		accesslist = "permit ip any ###IPSEITEA### 255.255.255.0";
+	}
+	ike_forward_rules = "udp 0.0.0.0:500 0.0.0.0:500",
+			"udp 0.0.0.0:4500 0.0.0.0:4500";
 
-	   }
+}
 
-	`
+`
 
 	ipsec_secret := `
 	   @###DYNDNSA### @###DYNDNSB### : PSK "###PSK###"
@@ -162,10 +159,14 @@ func main() {
 	bfipsec_conf = strings.Replace(bfipsec_conf, "###DYNDNSA###", *DYNDNSA, -1)
 	bfipsec_conf = strings.Replace(bfipsec_conf, "###IPSEITEA###", *ipSeiteAPtr, -1)
 	bfipsec_conf = strings.Replace(bfipsec_conf, "###IPSEITEB###", *ipSeiteBPtr, -1)
+	bfipsec_conf = strings.Replace(bfipsec_conf, "###PSK###", PSK, -1)
 
 	ipsec_secret = strings.Replace(ipsec_secret, "###PSK###", PSK, -1)
 	ipsec_secret = strings.Replace(ipsec_secret, "###DYNDNSB###", *DYNDNSB, -1)
 	ipsec_secret = strings.Replace(ipsec_secret, "###DYNDNSA###", *DYNDNSA, -1)
+
+	printSetup(*nameSeiteAPtr, *ipSeiteAPtr, *DYNDNSA)
+	printSetup(*nameSeiteBPtr, *ipSeiteBPtr, *DYNDNSB)
 
 	if *pconf {
 		printPSK(*DYNDNSA, *DYNDNSB, PSK)
